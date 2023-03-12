ef728cb47c22e4a3b24b59a29b92fbb73f564394
Yushi Sun <sunyushi9546@gmail.com>
2023-03-12 11:15:54 -0700

    complete the keywell

--- src/dactyl_keyboard/mykeyboard.clj
+++ src/dactyl_keyboard/mykeyboard.clj
@@ -8,22 +8,48 @@
 (def columns (range 0 6))
 (def rows (range 0 5))
 
-(def switchhole-width 14.1)
+(def switchhole-width 14.4)
 (def switchhole-notch-width 6)
 (def switchhole-notch-height 1.5)
 (def switchhole-notch-depth 2.75)
 (def plate-thickness 4)
 (def amoeba-king-support-radius 1.25)
+(def amoeba-king-support-length 2)
 (def amoeba-king-support-distance 19)
 (def keycap-space-width 19)
 (def keyhole-side-width (/ (- keycap-space-width switchhole-width) 2))
 
-(def top-wall (->> (cube keycap-space-width keyhole-side-width plate-thickness)
-                   (translate [0 (/ switchhole-width 2) 0])))
-(def left-wall (->> (cube keyhole-side-width keycap-space-width plate-thickness)
-                    (translate [(- (/ switchhole-width 2)) 0 0])))
+(def epsilon 0.01)
 
-(def keyhole (union top-wall left-wall))
+(defn add-epsilon [x] (+ x epsilon))
+(defn half-of [x] (/ x 2))
+(defn half-cylinder [r h]
+  (->> (difference (cylinder r h) (translate [0 (- (half-of r)) 0] (cube (add-epsilon (* 2 r)) r (add-epsilon h))))
+       (translate [0 0 (half-of h)])
+       (with-fn 30)))
+
+
+(def top-wall (->> (difference
+                    (->> (cube keycap-space-width keyhole-side-width plate-thickness)
+                         (translate [0 (half-of keyhole-side-width) (half-of plate-thickness)]))
+                    (->> (cube switchhole-notch-width (add-epsilon switchhole-notch-height) (add-epsilon switchhole-notch-depth))
+                         (translate [0 (half-of switchhole-notch-height) (half-of switchhole-notch-depth)])))
+                    ; Restore the translation center.
+                   (translate [0 (- (half-of keyhole-side-width)) 0])
+                   (translate [0 (half-of (+ switchhole-width keyhole-side-width)) 0])))
+
+(def left-wall (->> (cube keyhole-side-width keycap-space-width plate-thickness)
+                    (translate [(- (half-of (+ switchhole-width keyhole-side-width))) 0 (half-of plate-thickness)])))
+
+(def amoeba-king-support (->> (half-cylinder amoeba-king-support-radius amoeba-king-support-length)
+                              (rotate (- (half-of pi)) [0 0 1])
+                              (translate [(- (half-of amoeba-king-support-distance)) 0 (-  amoeba-king-support-length)])))
+
+(def half-keyhole (->> (union top-wall left-wall)
+                       (union amoeba-king-support)))
+
+(def keyhole (union half-keyhole
+                    (rotate pi [0 0 1] half-keyhole)))
 
 (def keyswitch-height 14.4) ;; Was 14.1, then 14.25
 (def keyswitch-width 14.4)
