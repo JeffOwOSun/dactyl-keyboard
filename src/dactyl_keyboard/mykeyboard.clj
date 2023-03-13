9f3a203c99f4cfa9e20829563c0694e5595506cd
Yushi Sun <sunyushi9546@gmail.com>
2023-03-12 17:30:08 -0700

    key holes for both hands done

--- src/dactyl_keyboard/mykeyboard.clj
+++ src/dactyl_keyboard/mykeyboard.clj
@@ -5,79 +5,149 @@
             [unicode-math.core :refer :all]
             [dactyl-keyboard.util :refer :all]))
 
-(def columns (range 0 6))
-(def rows (range 0 5))
+(binding [*center* false]
 
-(def switchhole-width 14.4)
-(def switchhole-notch-width 6)
-(def switchhole-notch-height 1.5)
-(def switchhole-notch-depth 2.75)
-(def plate-thickness 4)
-(def amoeba-king-support-radius 1.25)
-(def amoeba-king-support-length 2)
-(def amoeba-king-support-distance 19)
-(def keycap-space-width 19)
-(def keyhole-side-width (/ (- keycap-space-width switchhole-width) 2))
+  (def columns (range 0 6))
+  (def rows (range 0 5))
 
-(def epsilon 0.01)
+  (def switchhole-width 14.4)
+  (def switchhole-notch-width 6)
+  (def switchhole-notch-height 1.5)
+  (def switchhole-notch-depth 2.75)
+  (def plate-thickness 4)
+  (def amoeba-king-support-radius 1.25)
+  (def amoeba-king-support-length 2)
+  (def keycap-space-width 19)
+  (def keyhole-side-width (/ (- keycap-space-width switchhole-width) 2))
 
-(defn add-epsilon [x] (+ x epsilon))
-(defn half-of [x] (/ x 2))
-(defn half-cylinder [r h]
-  (->> (difference (cylinder r h) (translate [0 (- (half-of r)) 0] (cube (add-epsilon (* 2 r)) r (add-epsilon h))))
-       (translate [0 0 (half-of h)])
-       (with-fn 30)))
+  (def epsilon 0.01)
+
+  (defn add-epsilon [x] (+ x epsilon))
+  (defn half-of [x] (/ x 2))
+  (defn half-cylinder [r h]
+    (->> (difference (cylinder r h)
+                     (->> (cube (* 2 r) r h)
+                          (translate [(- r) 0 0])))
+         (rotate (/ pi 2) [0 0 1])
+         (with-fn 60)))
+
+  (def switchhole-notch
+    (cube switchhole-notch-width (add-epsilon switchhole-notch-height) (add-epsilon switchhole-notch-depth)))
+
+  (spit "things/switchhole-notch.scad" (write-scad switchhole-notch))
+
+  (def top-wall (->> (difference
+                      (->> (cube keycap-space-width keyhole-side-width plate-thickness)
+                           (translate [0 0 0]))
+                      (->> switchhole-notch
+                           (translate [(half-of (- keycap-space-width switchhole-notch-width)) (- keyhole-side-width switchhole-notch-height) 0])))))
+
+  (spit "things/top-wall.scad" (write-scad top-wall))
+
+  (def left-wall (cube keyhole-side-width keycap-space-width plate-thickness))
+
+  (spit "things/left-wall.scad" (write-scad left-wall))
+
+  (spit "things/half-cylinder.scad" (write-scad (half-cylinder amoeba-king-support-radius amoeba-king-support-length)))
+
+  (def amoeba-king-support (->> (half-cylinder amoeba-king-support-radius amoeba-king-support-length)
+                                (translate [0 (half-of keycap-space-width) (-  amoeba-king-support-length)])))
+
+  (def half-keyhole (->> (union top-wall left-wall)
+                         (union amoeba-king-support)))
+
+  (spit "things/half-keyhole.scad" (write-scad half-keyhole))
+
+  (def keyhole (union half-keyhole
+                      (->> half-keyhole
+                           (translate [(- (half-of keycap-space-width))
+                                       (- (half-of keycap-space-width))
+                                       0])
+                           (rotate pi [0 0 1])
+                           (translate [(half-of keycap-space-width)
+                                       (half-of keycap-space-width)
+                                       0]))))
+
+  ;; Four poles that delimit the corners of the keyhole. Used in filler generation.
+  (def top-left-pole (->> (cylinder epsilon plate-thickness)
+                          (translate [0 keycap-space-width 0])))
+  (def top-right-pole (->> (cylinder epsilon plate-thickness)
+                           (translate [keycap-space-width keycap-space-width 0])))
+  (def bottom-left-pole (cylinder epsilon plate-thickness))
+  (def bottom-right-pole (->> (cylinder epsilon plate-thickness)
+                              (translate [keycap-space-width 0 0])))
+
+  (spit "things/keyhole.scad" (write-scad keyhole))
+
+  (defn column-y-offsets [i]
+    (if (< i 0)
+      0
+      ([0 2 4 2 0 0] i)))
+
+  (defn translation-vector [col row] [(* keycap-space-width col) (+ (* (- keycap-space-width) row) (column-y-offsets col)) 0])
+
+  (defn key-place-flat [col row shape]
+    (translate (translation-vector col row) shape))
+
+  (def rotation-rad (* 0.10 pi))
+
+  (def rotation-radius (/ (half-of keycap-space-width) (Math/tan (/ rotation-rad 2))))
+
+  (defn thumb-key-place
+    "Places the num-th key in the thumb cluster to its designated position."
+    [num block]
+    (let
+     [center (fn [block] (translate [(- (half-of keycap-space-width)) 0 0] block))
+      turn (fn [rad, block] (rotate rad [0 0 1] block))
+      fanout (fn [block] (translate [0 rotation-radius 0] block))]
+      (->> block
+           center
+           fanout
+           (turn (* num rotation-rad)))))
+
+  (def thumb-cluster
+    (let [fanin (fn [block] (translate [0 (- rotation-radius) 0] block))
+          thumb-keyholes (union (->> keyhole
+                                     (thumb-key-place 0))
+                                (->> keyhole
+                                     (thumb-key-place 1))
+                                (->> keyhole
+                                     (thumb-key-place 2)))]
+      (->> thumb-keyholes
+           (fanin)
+           (translate [(half-of keycap-space-width) 0 0]))))
+
+  (def key-holes
+    (apply union
+           (for [col columns
+                 row rows
+                 :when (not
+                        (or
+                         (and (= col 0) (= row 4))
+                         (and (= col 1) (= row 4))
+                         (and (= col 4) (= row 4))
+                         (and (= col 5) (= row 4))))]
+             (->> keyhole
+                  (key-place-flat col row)))))
+
+  (def right-half
+    (let [left-padding 10
+          thumb-cluster-nudge 5]
+      (->> (union key-holes
+                  (->>
+                   thumb-cluster
+                   (key-place-flat 0 4)
+                   (translate [thumb-cluster-nudge 0 0])))
+           (translate [0 (- keycap-space-width) 0])
+           (rotate (* 0.15 pi) [0 0 1])
+           (translate [left-padding 0 0]))))
+
+  (spit "things/right-half.scad"
+        (write-scad right-half))
+
+  (def left-half (->> right-half
+                      (mirror [1 0 0])))
 
 
-(def top-wall (->> (difference
-                    (->> (cube keycap-space-width keyhole-side-width plate-thickness)
-                         (translate [0 (half-of keyhole-side-width) (half-of plate-thickness)]))
-                    (->> (cube switchhole-notch-width (add-epsilon switchhole-notch-height) (add-epsilon switchhole-notch-depth))
-                         (translate [0 (half-of switchhole-notch-height) (half-of switchhole-notch-depth)])))
-                    ; Restore the translation center.
-                   (translate [0 (- (half-of keyhole-side-width)) 0])
-                   (translate [0 (half-of (+ switchhole-width keyhole-side-width)) 0])))
-
-(def left-wall (->> (cube keyhole-side-width keycap-space-width plate-thickness)
-                    (translate [(- (half-of (+ switchhole-width keyhole-side-width))) 0 (half-of plate-thickness)])))
-
-(def amoeba-king-support (->> (half-cylinder amoeba-king-support-radius amoeba-king-support-length)
-                              (rotate (- (half-of pi)) [0 0 1])
-                              (translate [(- (half-of amoeba-king-support-distance)) 0 (-  amoeba-king-support-length)])))
-
-(def half-keyhole (->> (union top-wall left-wall)
-                       (union amoeba-king-support)))
-
-(def keyhole (union half-keyhole
-                    (rotate pi [0 0 1] half-keyhole)))
-
-(defn column-y-offsets [i]
-  (if (< i 0)
-    0
-    ([0 2 4 2 0 0] i)))
-
-(defn translation-vector [col row] [(* keycap-space-width col) (+ (* (- keycap-space-width) row) (column-y-offsets col)) 0])
-
-(defn key-place-flat [col row shape]
-  (translate (translation-vector col row) shape))
-
-(def thumb-cluster (->> (union (->> keyhole (key-place-flat 0 0))
-                               (->> keyhole (key-place-flat -1 0))
-                               (->> keyhole (key-place-flat -2 0)))
-                        (key-place-flat 0 4)))
-(def key-holes
-  (apply union
-         (for [col columns
-               row rows
-               :when (not
-                      (or
-                       (and (= col 0) (= row 4))
-                       (and (= col 1) (= row 4))
-                       (and (= col 4) (= row 4))
-                       (and (= col 5) (= row 4))))]
-           (->> keyhole
-                (key-place-flat col row)))))
-(def right-half (union key-holes thumb-cluster))
-
-(spit "things/mykeyboard.scad"
-      (write-scad right-half))
+  (spit "things/mykeyboard.scad"
+        (write-scad (union left-half right-half))))
